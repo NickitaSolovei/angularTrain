5edf6da5001fe55a226da84cabc7ba4b19253d0b
Nickita Solovei <https://github.com/NickitaSolovei>
2019-09-17 12:02:19 +0300

    "feat: add interfaces"

---
+++ angularTrain1/src/app/models/ICard.ts
@@ -0,0 +1,9 @@
+import IUser from './IUser';
+
+export default interface ICard {
+    id: string,
+    name: string,
+    description: string,
+    dueDate?: Date | string,
+    assignee?: IUser,
+}
